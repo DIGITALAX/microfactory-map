cec026cc8b7f4c6b88a21664c43c363812829ee7
digitalax_ <72898465+digitalax1@users.noreply.github.com>
2022-08-15 02:07:44 -0400

    add copy to clip

--- src/components/Map/MarkerInfo.jsx
+++ src/components/Map/MarkerInfo.jsx
@@ -1,15 +1,32 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState, useRef, useEffect} from 'react';
 import { AiFillCloseCircle } from 'react-icons/ai';
+import {FiCopy} from 'react-icons/fi';
 import {contextApi} from './../../pages/_app';
 import Draggable from 'react-draggable';
+import CopyToClipboard from 'react-copy-to-clipboard';
 
 function MarkerInfo(props) {
 
   const data = useContext(contextApi);
 
+  const [copy, setCopy] = useState(false);
+
+
   const closeMarkerInfo = () => {
     props.setClickedMarker(null);
+  };
+
+  const handleCopy = () => {
+      setCopy(true);
   }
+
+  useEffect(()=>{
+    setTimeout(()=>{
+      if (copy) 
+        setCopy(false);
+    }, 600)
+  }, [copy]);
+
 
   return props.clickedMarker ? (
     <div>
@@ -18,17 +35,28 @@ function MarkerInfo(props) {
         <Draggable enableUserSelectHack={false}>
           <div className='max-w-36 p-6 m-4 w-1/3 z-10 text-white text-sm font-space absolute ml-80 bottom-0 h-60 bg-lightGrey cursor-move'>
           <h1 className='cursor-text mb-4'>{props.clickedMarker.story}</h1>
-          <span className='mt-6 text-purple-400'>Prompt: </span>
+          <span className='mt-6 text-purple-400'>Prompt </span>
+          <CopyToClipboard
+          text={props.clickedMarker.prompt}
+          onCopy={handleCopy}
+          >
+          <FiCopy 
+            color='white'
+            className='absolute ml-14 top-[125px] cursor-pointer'
+            />
+          </CopyToClipboard>
+          {copy && <small className='absolute ml-7 mt-0.5 text-lime-200'>Copied</small>}
           <p className='cursor-text mt-1'>{props.clickedMarker.prompt}</p>
           <span class="relative h-3 w-3">
             <span class="animate-ping absolute h-3 w-3 rounded-full opacity-75 bg-amber-500 mt-6"></span>
             <span class="absolute inline-flex rounded-full h-3 w-3 bg-amber-500 mt-6"></span>
-            <p className='absolute top-5 ml-6 text-amber-500 font-sans'>{props.clickedMarker.nodeStatus}</p>
+            <a href="https://www.nicenode.xyz/" target="_blank" rel="noreferrer">
+              <p className='absolute top-5 ml-6 whitespace-nowrap text-amber-500 font-sans underline underline-offset-2'>{props.clickedMarker.nodeStatus}</p>
+            </a>
           </span>
           <AiFillCloseCircle 
           color='white'
           className='absolute top-0 right-0 p-0 m-2 cursor-pointer'
-          onClick={closeMarkerInfo}
           />
           </div>
         </Draggable>
@@ -36,12 +64,24 @@ function MarkerInfo(props) {
         <Draggable enableUserSelectHack={false}>
           <div className='max-w-36 p-6 m-4 w-1/3 z-10 text-white text-sm font-space absolute ml-28 bottom-0 h-60 bg-lightGrey cursor-move'>
           <h1 className='cursor-text mb-4'>{props.clickedMarker.story}</h1>
-          <span className='mt-6 text-purple-400'>Prompt: </span>
+          <span className='mt-6 text-purple-400'>Prompt </span>
+          <CopyToClipboard
+          text={props.clickedMarker.prompt}
+          onCopy={handleCopy}
+          >
+          <FiCopy 
+            color='white'
+            className='absolute ml-14 top-[125px] cursor-pointer'
+            />
+          </CopyToClipboard>
+          {copy && <small className='absolute ml-7 mt-0.5 text-lime-200'>Copied</small>}
           <p className='cursor-text mt-1'>{props.clickedMarker.prompt}</p>
           <span class="relative h-3 w-3">
             <span class="animate-ping absolute h-3 w-3 rounded-full opacity-75 bg-amber-500 mt-6"></span>
             <span class="absolute inline-flex rounded-full h-3 w-3 bg-amber-500 mt-6"></span>
-            <p className='absolute top-5 ml-6 text-amber-500 font-sans'>{props.clickedMarker.nodeStatus}</p>
+              <a href="https://www.nicenode.xyz/" target="_blank" rel="noreferrer">
+                <p className='absolute top-5 ml-6 whitespace-nowrap text-amber-500 font-sans underline underline-offset-2'>{props.clickedMarker.nodeStatus}</p>
+              </a>
           </span>
           <AiFillCloseCircle 
           color='white'
